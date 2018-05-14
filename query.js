0bf343197cbbabacba432b1e09a21bf8a9fa8040
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-05-14 21:45:53 +0700

    pollDB2 truly finished

--- query.js
+++ query.js
@@ -4,7 +4,7 @@ let db = new sqlite3.Database('./congress_poll_results.db');
 
 db.all(`SELECT * FROM congress_members WHERE grade_current < ? ORDER BY grade_current ASC`,[9],(err,rows)=>{
     if(err) throw err;
-    // console.log(rows)
+    console.log(rows)
 })
 
 
@@ -45,12 +45,37 @@ WITH politician_counts AS (
     
     `,(err,rows)=>{
     if (err) throw err;
+    console.log('\n')
     console.log(rows)
+})
+
+db.all(`
+    WITH topCounts AS(
+        WITH top_congress AS (
+            WITH limit_3 AS (
+                SELECT COUNT(*) 
+                AS votecount,politicianId 
+                FROM votes 
+                GROUP BY politicianId 
+                ORDER BY votecount 
+                DESC LIMIT 3
+            ) SELECT limit_3.*,congress_members.name 
+            FROM limit_3  
+            JOIN congress_members 
+            ON congress_members.id = limit_3.politicianId
+        ) SELECT top_congress.name,top_congress.votecount,votes.voterId 
+        FROM votes 
+        JOIN top_congress 
+        ON votes.politicianId = top_congress.politicianId
+    ) SELECT topCounts.name AS politician, topCounts.votecount AS votes, (voters.first_name || " " || voters.last_name) AS name FROM topCounts JOIN voters ON topCounts.voterId = voters.id ORDER BY votecount DESC`,(err,rows)=>{
+    if(err) throw err;
+    console.log('\n')
+    console.log(rows)
+
 })
 
 db.all(`SELECT COUNT(*) AS votecount, (voters.first_name || ' ' || voters.last_name) AS name FROM votes JOIN voters ON votes.voterId = voters.id GROUP BY votes.voterId HAVING votecount > 1 ORDER BY votecount DESC`,(err, rows)=>{
     if(err) throw err;
+    console.log('\n')
     console.log(rows)
 })
-
-//ambil semua, kemudian limit bahwa satu total Vote hanya bisa keluar 1
